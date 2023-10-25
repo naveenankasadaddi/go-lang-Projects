490b4d0978b4cbba8e584fd28770e73d26bb3774
nma <naveenank2001@gmail.com>
2023-10-25 15:47:22 +0530

    Saved

--- Arrays/main.go
+++ Arrays/main.go
@@ -10,11 +10,7 @@ func main() {
 
 func createArr() {
 	arr := [5]int{1, 2, 3, 4, 5}
-	//var i int=0
-	fmt.Println("Array elements :")
-	for i := 0; i < len(arr); i++ {
-		fmt.Print(arr[i])
-		fmt.Printf("\t")
-
+	for i, value := range arr {
+		fmt.Printf("Element at index %d is %d\n", i, value)
 	}
 }
